e0bcd53acada789eebf2bd207706ca72bb1b6f94
masayukeeeee <masayukeeeee@gmail.com>
2026-03-31 01:42:00 +0900

    fix bug

--- files/config/nvim/lua/msakai/plugins/treesitter.lua
+++ files/config/nvim/lua/msakai/plugins/treesitter.lua
@@ -1,35 +1,32 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "python",
-        "typescript",
-        "yaml",
-        "toml",
-        "c",
-        "cpp",
-        "rust",
-        "go",
-        "java",
-        "r",
-        "sql",
-        "sqlx",
-        "markdown"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  event = { "BufReadPre", "BufNewFile" }, -- 読み込みタイミングを明示してエラーを防ぐ
+  main = "nvim-treesitter.configs",       -- 実行するメインモジュールを指定
+  opts = {                                -- setup() に渡す引数を opts で定義
+    ensure_installed = {
+      "bash",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "typescript",
+      "yaml",
+      "toml",
+      "c",
+      "cpp",
+      "rust",
+      "go",
+      "java",
+      "r",
+      "sql",
+      "sqlx",
+      "markdown"
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
-
